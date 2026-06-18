599b2b7b8ffa167dd9a7bd9a3f64027e22aca70e
rathin-k <csvc.9b12.rathin@gmail.com>
2026-06-18 14:32:58 +0530

    Add Socket.IO client server connection

--- server/server.js
+++ server/server.js
@@ -1,11 +1,29 @@
 const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Server Running");
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
 });
 
-app.listen(5000, () => {
+io.on("connection", (socket) => {
+  console.log("User Connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected:", socket.id);
+  });
+});
+
+server.listen(5000, () => {
   console.log("Server running on port 5000");
 });
