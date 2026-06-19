90666b7067320018d6cd960fbf99ea409d2c2f78
rathin-k <csvc.9b12.rathin@gmail.com>
2026-06-19 14:54:31 +0530

    Create Home and Room pages

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <div>
+      <h1>Collaborative Coding Platform</h1>
+
+      <button>Create Room</button>
+    </div>
+  );
+}
+
+export default Home;
---
+++ client/src/pages/Room.jsx
@@ -0,0 +1,9 @@
+function Room() {
+  return (
+    <div>
+      <h1>Room Page</h1>
+    </div>
+  );
+}
+
+export default Room;
